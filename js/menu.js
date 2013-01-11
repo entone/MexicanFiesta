8c978398359d7c5a34f69a8d3b834e938fad9551
Christopher Coté <chris@entropealabs.com>
2013-01-11 22:33:16 +0000

    contact and menu pages

---
+++ js/menu.js
@@ -0,0 +1,77 @@
+var menu_display = {
+	"Appetizers":{
+		"description":""
+	},
+	"Salads & Soups":{
+		"description":""
+	},
+	"Tacos":{
+		"description":"Corn or flour tortilla tacos, deep-fried or soft, stuffed with the following and topped with crisp lettuce and mild salsa. Served with your choice of 2 sides Rice, Beans or Cole-Slaw (Add cheese to rice or beans $1.00)"
+	},
+	"Tostadas":{
+		"description":"Open, deep-fried corn tortillas topped with the following and shredded lettuce and mild salsa. Served with your choice of 1 side Rice, Beans or Coleslaw (Add cheese to rice or beans $1.00)"
+	},
+	"Burritos":{
+		"description":"Rolled flour tortillas stuffed with your choice of filling. Served with your choice of 2 sides Rice, Beans or Coleslaw (Add cheese to rice or beans $1.00)"
+	},
+	"Enchiladas":{
+		"description":"Rolled corn tortillas dipped in an Anaheim pepper sauce and stuffed with your choice of filling. Served with your choice of 2 sides Rice, Beans or Coleslaw (Add cheese to rice or beans $1.00)"
+	},
+	"Fajitas":{
+		"description":"All fajitas are served with your choice of soft corn or flour tortillas. Additional tortillas (3) for 50¢. Add cheese for $2.00."
+	},
+	"Egg Dishes":{
+		"description":"Served with rice, beans and tortillas"
+	},
+	"American Favorites":{
+		"description":""
+	},
+	"Sandwiches":{
+		"description":"All sandwiches served with fries. Add bacon to a sandwich for $1.00"
+	},
+	"Side Orders":{
+		"description":"Corn Chips - one basket per table complimentary for dine in customers. Each additional basket $2.00"
+	},
+	"Entrées":{
+		"description":"Fiesta entrées are served with rice and beans Add cheese to rice or beans for $1.00"
+	},
+	"Combo":{
+		"description":"Changes to Combo Dinners: 1st change no charge, each additional change $1.00, add cheese $2.00"
+	},
+	"Lunch and Drink Specials":{
+		"description":"Dine in only, No changes or substitutions on lunch specials."
+	},
+	"Daily Drink Specials":{
+		"description":"Served all day"
+	},
+	"Desserts":{
+		"description":""
+	},
+	"Margaritas":{
+		"description":"Change of tequila will increase price. Fruit Flavors - Strawberry, Raspberry, Blue, Mango, Peach, Strawberry Watermelon, Apple, Wild Berry Pomegranate"
+	},
+	"Tropical Cocktails":{
+		"description":""
+	},
+	"House Wines":{
+		"description":""
+	},	
+	"Coffee Drinks":{
+		"description":""
+	},
+	"Ice Cream Delights":{
+		"description":""
+	},
+	"Soft Drinks":{
+		"description":"Coca-Cola products (1 free refill) Coffee, Hot Tea, Raspberry Iced Tea, Hot chocolate, Milk, Apple Juice"
+	},	
+	"Mexican Bottle Beer":{
+		"description":"Dos Equis XX Amber or Lager, Negro Modelo, Modelo Especial Tecate, Corona, Corona Light, Sol, Tecate, Carta Blanca, Pacifico"
+	},
+	"Bottle Beer":{
+		"description":"Miller Lite, MGD, MGD 64, Miller High Life, Bud, Bud Light, Bud Light Lime, Bud, Bud Select 55, Michelob Ultra, Michelob Light, Sam Adams Boston Lager, Sam Adams Light, Coors Light, Molson, Amstel Light, Heineken, Heineken Light, Labatt Blue, Labatt Blue Light Smirnoff Ice, Mikes Hard Lemonade, Molson Ice"
+	},
+	"Draft Beer":{
+		"description":"12oz, 16oz, 20oz, Pitcher. Miller Lite, Bud Light, Labatt Blue, Killians Red Dos Equis Amber, Blue Moon, Cherry Wheat, Molson Canadian, Sam Adams Seasonal, Amstel Light"
+	}
+}
